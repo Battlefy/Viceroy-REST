fd6a0813361b1df9dac77049e87dab7e233e8485
shanejonas <jonas.shane@gmail.com>
2013-12-01 12:52:54 -0800

    use 123 instead of long string for dummy ID

--- demo/app.js
+++ demo/app.js
@@ -19,21 +19,24 @@ util.inherits(Tournament, viceroy.Model);
 viceroy.model('tournament', Tournament);
 
 viceroy.connect(function() {
-  var query = {_id: "52998de7effdc000000557a5"};
 
-  // GET http://localhost:8000/tournaments/52998de7effdc000000557a5
-  Tournament.findOne(query, function(err, tournament) {
+  // POST http://localhost:8000/tournaments => {name: 'My Tournament'}
+  Tournament.create({name: 'My Tournament'}, function(err, tournament) {});
+
+  // GET http://localhost:8000/tournaments/123
+  Tournament.findOne({_id: "123"}, function(err, tournament) {
     tournament.name = 'herp';
-    // PUT http://localhost:8000/tournaments/52998de7effdc000000557a5
+    // PUT http://localhost:8000/tournaments/123
     tournament.save(function(err, tournament){ })
   });
 
   // GET http://localhost:8000/tournaments
   Tournament.find({}, function(err, tournaments) { });
 
-  // POST http://localhost:8000/tournaments => {name: 'My Tournament'}
-  Tournament.create({name: 'My Tournament'}, function(err, tournament) { });
-
   // GET http://localhost:8000/tournaments?name=My%20Tournament
-  Tournament.find({name: 'My Tournament'}, function(err, tournament) { });
+  Tournament.find({name: 'My Tournament'}, function(err, tournament) {});
+
+  // DELETE http://localhost:8000/tournaments/123
+  Tournament.remove({_id: "123"}, function(err, tournament) {});
+
 });
